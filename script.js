e10f580ce7ad58f5c74fc6d055baa80b719b953b
Krishna Kumar <86869782+GitHubKrishnaKumar@users.noreply.github.com>
2023-02-18 23:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //your code here
 let ol = document.getElementById('data');
-
 fetch("https://api.github.com/repositories/1296269/issues?page=${1}&per_page=5.")
     .then((response) => response.json())
     .then((product) => {
@@ -9,44 +8,39 @@ fetch("https://api.github.com/repositories/1296269/issues?page=${1}&per_page=5."
             const element = product[i];
             console.log(product[i].title);
             ol.innerHTML += `<li>${product[i].title}</li>`
-            console.log(ol.innerHTML);
         }
     })
 
 let i = 1;
-
-let nextPage = document.getElementById('load_next');
-let prevPage = document.getElementById('load_prev');
-
-nextPage.addEventListener('click', () => {
+let next = document.getElementById('load_next');
+let prev = document.getElementById('load_prev');
+next.addEventListener('click', () => {
     i = i + 1;
     fetch(`https://api.github.com/repositories/1296269/issues?page=${i}&per_page=5.`)
         .then((response) => response.json())
         .then((product) => {
             document.getElementById('page_number').innerHTML = `Page Number ${i}`;
-            for (let z = 0; z < 5; z++) {
-                const element = product[z];
-                console.log(product[z].title);
-                ol.innerHTML = '';
-                ol.innerHTML += `<li>${product[z].title}</li>`
-                console.log(ol.innerHTML);
+            ol.innerHTML = '';
+            for (let j = 0; j< 5; j++) {
+                const element = product[j];
+                console.log(product[j].title);
+                
+                ol.innerHTML += `<li>${product[j].title}</li>`;
             }
         })
 })
-
-prevPage.addEventListener('click', () => {
+prev.addEventListener('click', () => {
     if (i === 1) i = 1;
     else i = i - 1;
     fetch(`https://api.github.com/repositories/1296269/issues?page=${i}&per_page=5.`)
-        .then((response) => response.json())
-        .then((product) => {
-            document.getElementById('page_number').innerHTML = `Page Number ${i}`;
-            for (let j = 0; j < 5; j++) {
-                const element = product[z];
-                console.log(product[j].title);
-                ol.innerHTML = '';
-                ol.innerHTML += `<li>${product[j].title}</li>`
-                console.log(ol.innerHTML);
-            }
-        })
+	.then((response) => response.json())
+	.then((product) => {
+		document.getElementById('page_number').innerHTML = `Page Number ${i}`;
+		ol.innerHTML = '';
+		for (let k = 0; k< 5; k++) {
+			const element = product[k];
+			console.log(product[k].title);
+			ol.innerHTML += `<li>${product[k].title}</li>`
+		}
+	})
 })
